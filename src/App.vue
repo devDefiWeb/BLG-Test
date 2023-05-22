a91451de1787fa12b4a3fc36b48e97ea6a3e38cd
devDefiWeb <dev.defi0720@gmail.com>
2023-05-22 04:31:43 -0400

    add fonrt

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,9 @@
     src: local("Inter"),
     url(./fonts/Inter/Inter-VariableFont_slnt,wght.ttf) format("truetype");
   }
+  @font-face {
+    font-family: "Bauhaus 93";
+    src: local("Bauhaus 93"),
+    url(./fonts/Bauhaus/BauhausBold.ttf) format("truetype");
+  }
 </style>
